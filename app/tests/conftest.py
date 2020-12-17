734b952fe12afed7f7f0c5e16eb9b4ffd8a9af05
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2020-12-17 10:42:28 +0100

    ensure pytest environment

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,6 +2,12 @@ import pytest
 from django.core.management import call_command
 
 
+@pytest.fixture(scope="session", autouse=True)
+def assert_dynaconf_pytest_environment():
+    from django.conf import settings
+    assert settings.ENVIRONMENT == 'pytest'
+
+
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
